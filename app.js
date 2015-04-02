70a1e8c79f65d395576c962ea192fc0e7aa563fe
HoaDinh <hoaminhdinh@gmail.com>
2015-04-02 22:45:24 +0700

    create

---
+++ app.js
@@ -0,0 +1,16 @@
+/**
+ * Created by hoadinh on 4/2/15.
+ */
+var express = require('express');
+var app = express();
+
+app.get('/', function(req, res) {
+    res.end('Hello World');
+});
+
+
+
+
+
+
+app.listen(8088);
